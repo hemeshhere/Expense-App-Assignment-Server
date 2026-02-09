23bbc8c7123553a21f72025e139560c79ef07d39
hemeshhere <hh9199939655@gmail.com>
2026-02-10 00:28:12 +0530

    Done with the structure of expenseDao

--- src/dao/expenseDao.js
+++ src/dao/expenseDao.js
@@ -1,5 +1,15 @@
 const Expense=require('../model/expense');
+const group = require('../model/group');
+const { getGroupByEmail } = require('./groupDao');
 const expenseDao={
+    createExpense: async (data)=>{
+        const expense= new Expense(data);
+        return await expense.save();
+    },
+    
+    getExpensesByGroupId: async (groupId)=>{
+        return await Expense.find({groupId}).sort({createdAt: -1});
+    },
 
 };
 module.exports=expenseDao;
